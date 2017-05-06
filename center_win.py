a6c0ae40e335df807d67d64cec174c6d0a18a8b8
Barry Buehne <Barry Buehne>
2017-05-06 00:27:30 -0500

    corrected error by adding missing function from module

--- center_win.py
+++ center_win.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QMessageBox
 
 
 class Example(QWidget):
