4e4b8929a7e17605a49d452b937f6cb66ef9a048
Barry Buehne <Barry Buehne>
2017-05-06 00:26:36 -0500

    Corrected error and added close verification

--- center_win.py
+++ center_win.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
 class Example(QWidget):
 
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
         self. initUI()
 
@@ -26,10 +26,22 @@ class Example(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, 'Message', 
+            "Are you sure you want to quit?", QMessageBox.Yes |
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
-    
